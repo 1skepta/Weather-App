d62d9e35bacd012e803f5044d689f5e535ff99bb
1skepta <prospaul992@gmail.com>
2024-12-11 11:16:43 +0000

    working

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,35 @@
 import { Stack } from "expo-router";
+import { Text } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#25292e",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#fff",
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: () => (
+            <Text
+              style={{
+                color: "#fff",
+                fontSize: 20,
+                paddingTop: 30,
+                fontWeight: 700,
+              }}
+            >
+              WEATHER APP
+            </Text>
+          ),
+        }}
+      />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
 }
